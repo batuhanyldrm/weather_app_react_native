45fbe2a71901dfec5caaaf9d83c79133fb893e45
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-06-13 23:29:52 +0300

    add search box slider

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Image } from 'react-native'
 import { theme } from '../theme'
 import { MagnifyingGlassIcon } from "react-native-heroicons/outline" 
 
 export default function HomeScreen() {
+
+  const [showSearch, setShowSearch] = useState(false)
+
   return (
     <View style={{ flex: 1, position: 'relative' }}>
       <StatusBar style='light' />
@@ -15,14 +18,19 @@ export default function HomeScreen() {
       <SafeAreaView className="flex flex-1">
         <View style={{height:"7%"}} className="mx-4 relative z-50" >
           <View className="flex-row justify-end items-center rounded-full"
-            style={{backgroundColor: theme.bgWhite(0.2)}}
+            style={{backgroundColor: showSearch ? theme.bgWhite(0.2) : "transparent"}}
           >
-            <TextInput
-              placeholder='Search city'
-              placeholderTextColor={'rgb(255,255,255)'}
-              className="pl-6 h-10 flex-1 text-base text-white"
-            />
+            {
+              showSearch ? (
+                <TextInput
+                placeholder='Search city'
+                placeholderTextColor={'rgb(255,255,255)'}
+                className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+              />
+              ): null
+            }
             <TouchableOpacity style={{backgroundColor: theme.bgWhite(0.3)}}
+              onPress={() => setShowSearch(!showSearch)}
               className="rounded-full p-3 m-1"
             >
               <MagnifyingGlassIcon size={"25"} color={"white"} />
