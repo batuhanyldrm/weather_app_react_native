8f721d5c51709c89e7999f2bac4d6dbf2a25ddf0
batuhan yıldırım <yildirimbatu52@gmail.com>
2023-06-13 23:14:01 +0300

    add hpme page search icon and search box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Image } from 'react-native'
+import { theme } from '../theme'
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline" 
 
 export default function HomeScreen() {
   return (
@@ -10,6 +12,24 @@ export default function HomeScreen() {
       <Image blurRadius={70} source={require('../assets/bg.png')}  
         style={{ position: 'absolute', height: '100%', width: '100%' }}
       /> 
+      <SafeAreaView className="flex flex-1">
+        <View style={{height:"7%"}} className="mx-4 relative z-50" >
+          <View className="flex-row justify-end items-center rounded-full"
+            style={{backgroundColor: theme.bgWhite(0.2)}}
+          >
+            <TextInput
+              placeholder='Search city'
+              placeholderTextColor={'rgb(255,255,255)'}
+              className="pl-6 h-10 flex-1 text-base text-white"
+            />
+            <TouchableOpacity style={{backgroundColor: theme.bgWhite(0.3)}}
+              className="rounded-full p-3 m-1"
+            >
+              <MagnifyingGlassIcon size={"25"} color={"white"} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
